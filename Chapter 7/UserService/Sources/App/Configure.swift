cae25c97528b88f603cd516c947285fe27962802
Ralph Kuepper <ralph.kuepper@skelpo.com>
2020-03-15 22:20:42 -0400

    Removed double CORSMiddleware.

--- Chapter 7/UserService/Sources/App/Configure.swift
+++ Chapter 7/UserService/Sources/App/Configure.swift
@@ -8,8 +8,6 @@ import SendGrid
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.middleware.use(CORSMiddleware())
     
     guard let jwksString = Environment.process.JWKS else { fatalError("No value was found at the given public key environment 'JWKS'")
     }
